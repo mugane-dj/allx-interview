fd0f42c1d417b190e70d9b1a4513cbd33e2cb947
mugane-dj <muganedev25@gmail.com>
2023-08-12 20:59:01 +0300

    0x0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,29 +8,23 @@ minOperations - calculates the minimum number of operations required to obtain
 
 def minOperations(n: int) -> int:
     """
-    The function `minOperations` calculates the minimum number of
-    operations required to obtain a string of length `n` consisting
-    of the character "H" using the operations of copying all and pasting.
+    The function `minOperations` calculates the minimum number of operations
+    required to reach a given number `n` using only three operations: adding 1,
+    multiplying by 2, and multiplying by 3.
 
-    :param n: The parameter `n` represents the number of characters
-              you want to obtain in the string
+    :param n: The parameter `n` represents the target number for which we want
+              to find the minimum number of operations
     :type n: int
-    :return: the minimum number of operations required to obtain a
-             string of length n, where the operations allowed are
-             copying all the characters and pasting them.
+    :return: the minimum number of operations required to reach the number `n`.
     """
-    copy_all = 0
-    paste = 0
-    char_string = "H"
+    table = [n] * (n + 1)
+    table[1] = 0
 
-    while (n > 1):
-        if (len(char_string) % 2):
-            char_string *= 2
-            n //= 2
-        else:
-            char_string += "H"
-            n -= 1
+    for i in range(1, n):
+        table[i + 1] = min(table[i + 1], table[i] + 1)
+        if i * 2 <= n:
+            table[i * 2] = min(table[i] + 1, table[i * 2])
+        if i * 3 <= n:
+            table[i * 3] = min(table[i] + 1, table[i * 3])
 
-        copy_all += 1
-        paste += 1
-    return copy_all + paste
+    return table[n]
