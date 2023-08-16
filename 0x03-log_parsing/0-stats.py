75b8c72e0b0b09cb28758eb52101228707d6b4a0
mugane-dj <muganedev25@gmail.com>
2023-08-16 12:03:02 +0300

    0x0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -33,4 +33,4 @@ if __name__ == "__main__":
         print("File size: {}".format(total_size))
         for status, count in ip_counts.items():
             if count > 0:
-                print("{}:{}".format(status, count))
+                print("{}: {}".format(status, count))
