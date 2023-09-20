edef15ff22db665b0fbadda186e3709ce60af487
mugane-dj <muganedev25@gmail.com>
2023-09-20 16:09:28 +0300

    0x0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Least coins module
+"""
+from typing import List
+
+
+def makeChange(coins: List[int], total: int) -> int:
+    """
+    Determines the fewest number of coins needed
+    to meet the given amount `total`
+    """
+    if total <= 0:
+        return 0
+    m = len(coins)
+
+    # create solution matrix
+    a = [[0 for _ in range(total + 1)] for _ in range(m)]
+
+    for i in range(m):
+        for j in range(total + 1):
+            if i == 0:
+                if coins[i] > j:
+                    a[i][j] = 0
+                else:
+                    a[i][j] = 1 + a[i][j - coins[i]]
+            else:
+                if coins[i] > j:
+                    a[i][j] = a[i - 1][j]
+                else:
+                    a[i][j] = min(a[i - 1][j], 1 + a[i][j - coins[i]])
+    return a[m - 1][total]
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,1 @@
+# Least Coins Algorithm
