0e866b164a7551ad8d3b7e15d5d5162aa196ed54
mugane-dj <muganedev25@gmail.com>
2023-09-22 16:51:45 +0300

    0x0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,25 +10,18 @@ def makeChange(coins: List[int], total: int) -> int:
     Determines the fewest number of coins needed
     to meet the given amount `total` using dynamic programming approach
     """
-    if total <= 0:
-        return 0
     m = len(coins)
-
-    if m == 0:
-        return -1
-
-    for coin in coins:
-        if total < coin:
-            return -1
 
     # create solution matrix
     a = [[0 for _ in range(total + 1)] for _ in range(m)]
 
     for i in range(m):
         for j in range(total + 1):
-            if i == 0:
+            if i == 0 and j == 0:
+                a[i][j] = 0
+            elif i == 0:
                 if coins[i] > j:
-                    a[i][j] = 0
+                    a[i][j] = -1
                 else:
                     a[i][j] = a[i][j - coins[i]] + 1
             else:
