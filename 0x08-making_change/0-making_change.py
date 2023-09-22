89c26021c0493c0f3a0482541b27ca31c6006458
mugane-dj <muganedev25@gmail.com>
2023-09-22 11:06:50 +0300

    0x0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -17,6 +17,10 @@ def makeChange(coins: List[int], total: int) -> int:
     if m == 0:
         return -1
 
+    for coin in coins:
+        if total < coin:
+            return -1
+
     # create solution matrix
     a = [[0 for _ in range(total + 1)] for _ in range(m)]
 
