55b86146aa137742352efe59e01f6e06ea82f504
muganedj <muganes25@gmail.com>
2023-10-04 21:13:34 +0300

    optimized version

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,29 +14,11 @@ def makeChange(coins: List[int], total: int) -> int:
     :return: fewest number of coins needed to meet `total`
              -1 if `total` cannot be met by any number of coins
     """
-    m = len(coins)
+    result = []
+    coins.sort(reverse=True)
+    for coin in coins:
+        while total // coin > 0:
+            result.append(coin)
+            total -= coin
 
-    if m == 0:
-        return -1
-
-    if total <= 0:
-        return 0
-
-    # create solution matrix
-    a = [[0 for _ in range(total + 1)] for _ in range(m)]
-
-    for i in range(m):
-        for j in range(total + 1):
-            if i == 0 and j == 0:
-                a[i][j] = 0
-            elif i == 0:
-                if coins[i] > j:
-                    a[i][j] = -1
-                else:
-                    a[i][j] = a[i][j - coins[i]] + 1
-            else:
-                if coins[i] > j:
-                    a[i][j] = a[i - 1][j]
-                else:
-                    a[i][j] = min(a[i - 1][j], 1 + a[i][j - coins[i]])
-    return a[m - 1][total]
+    return len(result) if total == 0 else -1
